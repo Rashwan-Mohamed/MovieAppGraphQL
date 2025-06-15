533ef11f3f899c2a01b6563d2c4e1e265798d236
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-15 15:46:06 +0300

    Fix3 vercel.json runtime issue

--- api/graphql.js
+++ api/graphql.js
@@ -1,6 +1,5 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { config } from 'dotenv';
 import { MovieAPI } from '../server/src/datasources/movie-api.js';
 import {typeDefs} from '../server/src/schema.js';
 import {resolvers} from '../server/src/resolvers.js';
