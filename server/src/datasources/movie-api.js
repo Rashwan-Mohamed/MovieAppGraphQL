f17f98d06769efbe9be2237626d26da2944a18c8
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-16 14:26:30 +0300

    Try3

--- server/src/datasources/movie-api.js
+++ server/src/datasources/movie-api.js
@@ -1,39 +1,40 @@
-/* eslint-env node */
-
-import "dotenv/config";
 import { RESTDataSource } from "@apollo/datasource-rest";
-import 'dotenv/config';
 
-const API_TOKEN = process.env.TMDB_API_TOKEN; // Use process.env, not import.meta.env
-
-
+const API_TOKEN = process.env.TMDB_API_TOKEN;
 
 export class MovieAPI extends RESTDataSource {
   baseURL = "https://api.themoviedb.org/3/";
 
-  willSendRequest(options) {
-    options.headers = options.headers || {};
-    options.headers["Authorization"] = `Bearer ${API_TOKEN}`;
-    options.headers["accept"] = "application/json";
-  }
-
   getNowPlayingMovies(series, whatShow, page = 1) {
-
-    let baseUrl = ``;
-    let queryParams = new URLSearchParams({
+    const queryParams = new URLSearchParams({
       language: "en-US",
-      page: page,
+      page: page.toString(),
     });
+
+    let baseUrl = "";
     if (series && whatShow === "trending") {
       baseUrl = `trending/tv/week`;
     } else {
       baseUrl = `${series ? "tv" : "movie"}/${whatShow}`;
     }
+
     const query = `${baseUrl}?${queryParams.toString()}`;
-    return this.get(query);
+    return this.get(query, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+        accept: "application/json",
+      },
+    });
   }
+
   getSearchResult(series, searchQuery) {
-    let query = `search/${series === "series" ? "tv" : "movie"}?query=${searchQuery}&include_adult=false&language=en-US&page=1`;
-    return this.get(query);
+    const type = series === "series" ? "tv" : "movie";
+    const query = `search/${type}?query=${encodeURIComponent(searchQuery)}&include_adult=false&language=en-US&page=1`;
+    return this.get(query, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+        accept: "application/json",
+      },
+    });
   }
 }
