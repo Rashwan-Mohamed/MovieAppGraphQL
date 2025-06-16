e9254f74fd77ab2c9f6f00d30ea264fdc7fab338
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-16 14:42:26 +0300

    Try5

--- server/src/datasources/movie-api.js
+++ server/src/datasources/movie-api.js
@@ -1,7 +1,13 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 
 const API_TOKEN = process.env.TMDB_API_TOKEN;
-
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDgzYTdhN2M5YjhlZGNmN2RlZDYwNmU3ZjA5Mjg1NiIsIm5iZiI6MTY3NjczMTE0MS40MzkwMDAxLCJzdWIiOiI2M2YwZTMwNWEyNGM1MDAwODQ4YzkyZWUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0ps3nbsJzYSZFnBf4KKb8DmG6An5hzSe5SKyT5RsqdQ'
+  }
+};
 export class MovieAPI extends RESTDataSource {
   baseURL = "https://api.themoviedb.org/3/";
 
@@ -20,23 +26,12 @@ export class MovieAPI extends RESTDataSource {
     }
 
     const query = `${baseUrl}?${queryParams.toString()}`;
-    return this.get(query, {
-      headers: {
-        Authorization: `Bearer ${API_TOKEN}`, // Must match the working curl
-        accept: "application/json",
-
-      },
-    });
+    return this.get(query, options);
   }
 
   getSearchResult(series, searchQuery) {
     const type = series === "series" ? "tv" : "movie";
     const query = `search/${type}?query=${encodeURIComponent(searchQuery)}&include_adult=false&language=en-US&page=1`;
-    return this.get(query, {
-      headers: {
-        Authorization: `Bearer ${API_TOKEN}`, // Must match the working curl
-        accept: "application/json",
-      },
-    });
+    return this.get(query, options);
   }
 }
