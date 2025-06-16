f5530f89ccc830c39603decfde3f517859c222c5
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-16 16:09:31 +0300

    expose

--- server/src/datasources/movie-api.js
+++ server/src/datasources/movie-api.js
@@ -1,18 +1,18 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 
 const API_TOKEN = process.env.TMDB_API_TOKEN;
-
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer yJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDgzYTdhN2M5YjhlZGNmN2RlZDYwNmU3ZjA5Mjg1NiIsIm5iZiI6MTY3NjczMTE0MS40MzkwMDAxLCJzdWIiOiI2M2YwZTMwNWEyNGM1MDAwODQ4YzkyZWUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0ps3nbsJzYSZFnBf4KKb8DmG6An5hzSe5SKyT5RsqdQ`
+  }
+};
 export class MovieAPI extends RESTDataSource {
   baseURL = "https://api.themoviedb.org/3/";
 
   getNowPlayingMovies(series, whatShow, page = 1) {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_TOKEN}`
-      }
-    };
+
     const queryParams = new URLSearchParams({
       language: "en-US",
       page: page.toString(),
@@ -31,13 +31,7 @@ export class MovieAPI extends RESTDataSource {
   }
 
   getSearchResult(series, searchQuery) {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_TOKEN}`
-      }
-    };
+
     const type = series === "series" ? "tv" : "movie";
     const query = `search/${type}?query=${encodeURIComponent(searchQuery)}&include_adult=false&language=en-US&page=1`;
     return this.get(query, options);
