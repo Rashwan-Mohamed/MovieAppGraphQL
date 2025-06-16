2ee7d52b62f1964dfe83f93e67369ea290a1309f
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-16 11:18:42 +0300

    updateEndPoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { React } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -11,33 +11,31 @@ import ViewMore from "./features/mainSlice/ViewMore.jsx";
 import { Series } from "./Components/Series.jsx";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://movie-app-five-opal.vercel.app/api",
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-      {" "}
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route index element={<Home />}></Route>
-              <Route path="/:Series" element={<Series />}></Route>
-              <Route path="/movies/:id" element={<Movie />}></Route>
-              <Route
-                path="/Series/:id"
-                element={<Movie series={true} />}
-              ></Route>
-              <Route path="/view/:genre" element={<ViewMore />}></Route>
-              <Route
-                path="/viewSeries/:genre"
-                element={<ViewMore series={true} />}
-              ></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </ApolloProvider>
-  </StrictMode>
+  <ApolloProvider client={client}>
+    {" "}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/:Series" element={<Series />}></Route>
+            <Route path="/movies/:id" element={<Movie />}></Route>
+            <Route
+              path="/Series/:id"
+              element={<Movie series={true} />}
+            ></Route>
+            <Route path="/view/:genre" element={<ViewMore />}></Route>
+            <Route
+              path="/viewSeries/:genre"
+              element={<ViewMore series={true} />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 );
