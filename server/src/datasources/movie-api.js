961f70dcb9be1aeaae559b45ce0dcd256555365b
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-16 14:09:19 +0300

    remove log

--- server/src/datasources/movie-api.js
+++ server/src/datasources/movie-api.js
@@ -18,7 +18,6 @@ export class MovieAPI extends RESTDataSource {
   baseURL = "https://api.themoviedb.org/3/";
 
   getNowPlayingMovies(series, whatShow, page = 1) {
-    console.log(API_TOKEN, 'ASSURE TOKEN');
 
     let baseUrl = ``;
     let queryParams = new URLSearchParams({
